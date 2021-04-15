059bc673735842035ebded8eb980d5b6b648a36d
Gabriel Forsythe <gabe.forsythe@gmail.com>
2021-04-15 11:11:09 -0700

    reconfig app.js on the front end

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,29 +1,11 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
-
-  render() {
+export default function App () {
+  
     return (
       <div className="App">
         <h1>{ this.state.message }</h1>
@@ -32,7 +14,7 @@ class App extends Component {
         </button>        
       </div>
     );
-  }
+
 }
 
-export default App;
+
